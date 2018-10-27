549faa31b699f3e8498e462d339de77de4b37c89
Zulwiyoza Putra <zulwiyozaputra@gmail.com>
2018-10-27 16:35:48 +0700

    Initial Commit

---
+++ BubbleSox/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BubbleSox
+//
+//  Created by Zulwiyoza Putra on 27/10/18.
+//  Copyright © 2018 Zulwiyoza Putra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
